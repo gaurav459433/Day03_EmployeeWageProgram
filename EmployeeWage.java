d70d9dd6fc1bc0e5e5cfa1616ce1901e337ceadd
gaurav459433 <gaurav459433@gmail.com>
2023-02-23 12:54:41 +0530

    UC2 - Daily Employee Wage Logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,12 +4,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
         int FULLTIME = 1;
+        int EMP_WAGE_PH  = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == FULLTIME) {
-            System.out.println("Employee Is Present");
+            empHrs = 8;
         }
         else {
-            System.out.println("Employee Is Absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_WAGE_PH;
+        System.out.println("Employee Daily Wage is :" +empWage);
     }
 }
