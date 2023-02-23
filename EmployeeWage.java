7d0eb91f1ac162eb89c94e0da422a28b022c6934
gaurav459433 <gaurav459433@gmail.com>
2023-02-23 13:34:25 +0530

    UC4 - Employee Wage Using SwitchCase Statement Logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,23 +1,29 @@
 package com.bridgelabz.assignmentsrfp259.day03.employeewage;
 
 public class EmployeeWage {
+    public static final int FULLTIME = 1;
+    public static final int PARTTIME = 2;
+    public static final int EMP_WAGE_PH  = 20;
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
-        int FULLTIME = 1;
-        int PARTTIME = 2;
-        int EMP_WAGE_PH  = 20;
+
         int empHrs = 0;
         int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == FULLTIME) {
+        switch ((int) empCheck) {
+            case FULLTIME :
             empHrs = 8;
             empWage = empHrs * EMP_WAGE_PH;
             System.out.println("Employee Daily Wage is :" +empWage);
-        } else if (empCheck == PARTTIME) {
+            break;
+
+            case PARTTIME :
             empHrs = 4;
             empWage = empHrs * EMP_WAGE_PH;
             System.out.println("Employee PartTime Wage is :" +empWage);
-        } else {
+            break;
+
+            default :
             empHrs = 0;
             empWage = empHrs * EMP_WAGE_PH;
             System.out.println("Employee Is Absent So Wage is :" +empWage);
