01b69664d2f6b6ce88295651705c1b01ef4b73d6
gaurav459433 <gaurav459433@gmail.com>
2023-02-23 12:37:24 +0530

    UC1 - Employee Present Absent Logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,5 +3,13 @@ package com.bridgelabz.assignmentsrfp259.day03.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
+        int FULLTIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == FULLTIME) {
+            System.out.println("Employee Is Present");
+        }
+        else {
+            System.out.println("Employee Is Absent");
+        }
     }
 }
